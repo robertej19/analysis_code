e936d298df4822016c35df18a238f9bdc195337d
Andrey Kim <drewkenjo@users.noreply.github.com>
2019-08-12 14:54:17 -0400

    ep counts script update with mondb

--- examples/ep_count.groovy
+++ examples/ep_count.groovy
@@ -15,13 +15,21 @@ def target = new Particle(2212, 0,0,0)
 for(fname in args) {
 int run = fname.split("/")[-1].split('\\.')[0][-4..-1].toInteger()
 int fnum = fname.split(".evio.")[-1].split('-')[0].toInteger()
-println fnum
 
 def reader = new HipoDataSource()
 reader.open(fname)
 
+float fcmin = Float.MAX_VALUE, fcmax = 0
+int counts = 0
 while(reader.hasEvent()) {
   def event = reader.getNextEvent()
+  if (event.hasBank("RUN::scaler")) {
+    def scalerBank = event.getBank("RUN::scaler")
+    def fc = scalerBank.getFloat('fcupgated', 0)
+    if (fc<fcmin) fcmin = fc
+    if (fc>fcmax) fcmax = fc
+  }
+
   if (event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter")) {
     def (ele, pro) = EP.getEP(event)
     if(ele!=null) {
@@ -36,6 +44,7 @@ while(reader.hasEvent()) {
       def phi = enorm.theta(pnorm)
       hemm2fi['before'].fill(phi, eX.mass2())
       if(phi>176 && eX.mass2()<1.5) {
+        counts++
         hemm2fi['after'].fill(phi, eX.mass2())
       }
     }
@@ -49,4 +58,9 @@ out.mkdir('/epcounts')
 out.cd('/epcounts')
 hemm2fi.values().each{out.addDataSet(it)}
 out.writeFile('ep_count_hists_'+fname.split("/")[-1])
+
+def sql = Sql.newInstance("jdbc:sqlite:/home/kenjo/elastic_mondb.db", "org.sqlite.JDBC")
+//sql.execute("drop table if exists elastic")
+//sql.execute("create table elastic (run integer, file integer, fcmin float, fcmax float, counts integer, primary key (run, file))")
+sql.execute("insert or replace into elastic values ($run, $fnum, $fcmin, $fcmax, $counts)")
 }
