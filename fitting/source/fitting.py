9e30f64749781bc1299235066bf00494c22acfba
Robert Johnston <robertej@ifarm1402.jlab.org>
2019-08-29 09:40:37 -0400

    renamed

--- fitting/source/fitting.py
+++ fitting/source/fitting.py
@@ -27,13 +27,13 @@ h0 = ff.Get("5038_H_proton_DeltaBeta_momentum_S2")
 def fitter(hist,mincut,maxcut,energy_conv):
 	f1 = ROOT.TF1('f1', 'gaus',-0.1,0.1)
 	h1 = h0.ProjectionY("Histogram",mincut,maxcut,"[cutg]")
-	h1.Fit(f1, 'R')
+	#h1.Fit(f1, 'R')
 	qq = ROOT.TSpectrum(2*3)
 	nfound = qq.Search(h1,1,"new")
 
-	amp = f1.GetParameter(0)
-	mean = f1.GetParameter(1)
-	sigma = f1.GetParameter(2)
+	amp = 0# f1.GetParameter(0)
+	mean = 0# f1.GetParameter(1)
+	sigma = 0#f1.GetParameter(2)
 
 	c1 = ROOT.TCanvas('c1','c1',1100,800)
 	#TCanvas *c1 = new TCanvas("c1", "c1",1024,44,926,686);
@@ -41,6 +41,20 @@ def fitter(hist,mincut,maxcut,energy_conv):
 	#ROOT.gStyle.SetOptTitle(0)
 	h1.SetTitle("Projection from {0} GeV to {1} GeV".format(round(mincut*energy_conv,2),round(maxcut*energy_conv,2)))
 	h1.Draw("colz")
+
+		print(type(h1))
+#print("h is")
+#print(h1)
+#f1.SetParameter(1, h1.GetBinCenter(h1.GetMaximumBin()))
+#f1.SetParameter(2, h1.GetRMS())
+#mu,sig = [f1.GetParameter(i) for i in range(1,3)]
+
+#f1.SetRange(mu-2.5*abs(sig), mu+2.5*abs(sig))
+#h1.Fit(f1)
+mu,sig = [f1.GetParameter(i) for i in range(1,3)]
+
+#f1.SetRange(mu-2.5*abs(sig), mu+2.5*abs(sig))
+h1.Fit(f1, 'R')
 
 
 	#pl = ROOT.TPaveLabel(.05,100,.95,200,"New Title","br")
@@ -55,7 +69,7 @@ for i in range(0,20):
 	amps.append(amp)
 	means.append(mean)
 	sigmas.append(sigma)
-
+"""
 print("sigmas")
 print(sigmas)
 print("means")
@@ -68,4 +82,4 @@ print(len(x))
 print(len(sigmas))
 
 #plt.plot(x,means)
-#plt.show
+#plt.show"""
