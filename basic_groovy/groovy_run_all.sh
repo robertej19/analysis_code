b70f9eb9e712b80e97f0458f52c2c0ae0259bffa
Robert Johnston <robertej@ifarm1402.jlab.org>
2019-09-02 18:28:12 -0400

    ""

--- basic_groovy/groovy_run_all.sh
+++ basic_groovy/groovy_run_all.sh
@@ -1,2 +1,3 @@
-$COATJAVA/bin/run-groovy $1 `find /volatile/clas12/rg-a/production/recon/pass1/dst/v2/005039/dst_clas_005039.evio.*`
+$COATJAVA/bin/run-groovy $1 `find /volatile/clas12/rg-a/production/recon/pass1/dst/v2/00504*`
+#`find /volatile/clas12/rg-a/production/recon/pass1/dst/v2/005039/dst_clas_005039.evio.*`
 
