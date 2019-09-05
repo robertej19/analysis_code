9a2ebd819de8e2a90d8948c5b0cf5574a34986c1
robertej19 <robertej@mit.edu>
2019-09-04 22:18:42 -0400

    ""

--- basic_groovy/v5_Protons.groovy
+++ basic_groovy/v5_Protons.groovy
@@ -111,8 +111,8 @@ for(fname in args) {
 			//	}
 
 			if(scint_detectors[particle_index]==4){
-				println("particle status is: ${particle_stati[particle_index]}")
-				println("layer and sector are: "+ scint_sectors[particle_index]+" layer: "+
+				printer("particle status is: ${particle_stati[particle_index]}")
+				printer("layer and sector are: "+ scint_sectors[particle_index]+" layer: "+
 					scint_layers[particle_index]+" sector: "+scint_sectors[particle_index])
 				Hist_beta_p_ctof.fill(particle_momentum,beta_recon)
 			}
