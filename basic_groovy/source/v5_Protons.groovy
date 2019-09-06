c1b7346f0b7a9832b6acc6d2b56538ab11602939
robertej19 <robertej@mit.edu>
2019-09-05 22:51:33 -0400

    ""

--- basic_groovy/source/v5_Protons.groovy
+++ basic_groovy/source/v5_Protons.groovy
@@ -85,10 +85,10 @@ for(fname in args) {
 		def recon_Cal = event.getBank("REC::Calorimeter")
 		def recon_Scint = event.getBank("REC::Scintillator")
 
-		if(!(recon_Particles.getInt("pid",0)==11)){
-			printer("Event had trigger particle with pID not equal to 11, skipping",0)
-			continue
-		}
+		//if(!(recon_Particles.getInt("pid",0)==11)){
+	//		printer("Event had trigger particle with pID not equal to 11, skipping",0)
+//			continue
+//		}
 
 		def particle_momenta = ['x','y','z'].collect{recon_Particles.getFloat('p'+it)}.transpose().collect{Math.sqrt(it.collect{x->x*x}.sum())}
 		def particle_stati = recon_Particles.getInt('status')
