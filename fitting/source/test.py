be40c2f52b27841c476c2da0ff99e7883911368d
robertej19 <robertej@mit.edu>
2019-09-05 22:50:29 -0400

    ""

--- fitting/source/test.py
+++ fitting/source/test.py
@@ -6,7 +6,6 @@ import ROOT
 
 ff = ROOT.TFile(sys.argv[1])
 
-
 for kk in ff.GetListOfKeys():
   obj = kk.ReadObj()
   print(obj.__class__)
@@ -15,16 +14,32 @@ for kk in ff.GetListOfKeys():
   print(obj.GetNbinsY())
   #print("value of x bin is: ")
   #print(obj.GetXaxis().FindBin(100))#obj.FindLastBinAbove()))
-
-
-
 #h1 = ff.Get("5038_H_proton_DeltaBeta_momentum_S2")#.ProjectionY("cutg",0,40,"[cutg]")#5038_Hist_deltaB_psec1_layer1")#.ProjectionY("cutg",0,40,"[cutg]")
 
-h1 = ff.Get("5039_Hist_deltaB_psec1_layer1")#.ProjectionY("cutg",0,40,"[cutg]")
+#h1 = ff.Get("5039_Hist_deltaB_psec1_layer1")#.ProjectionY("cutg",0,40,"[cutg]")
 
+type1 = "5039_Hist_beta_psec1_layer1"
+type2 = "5039_Hist_deltaB_psec1_layer1"
+type3 = "5039_Hist_beta_p2sec1_layer1"
+type4 = "5039_Hist_beta_p_ctof"
 
+typeX = sys.argv[2]
+print("type to print is {0}".format(typeX))
+
+if typeX==1:
+  type = type1
+elif typeX==2:
+  type = type2
+elif typeX==3:
+  type = type3
+elif typeX==4:
+  type = type4
+else:
+  print("type not found, ISSUE!!!!")
+
+h1 = ff.Get(type)
 
 c1 = ROOT.TCanvas('c1','c1',1100,800)
 c1.SetLogz()
 h1.Draw("colz")
-c1.Print("protons.pdf")
+c1.Print("full_plot.pdf")
