41b6b4af9864b2f433f17a04d59ff6f945d6bb3d
robertej19 <robertej@mit.edu>
2020-03-06 21:13:21 -0500

    ""

--- groovy/source/prime_sector.groovy
+++ groovy/source/prime_sector.groovy
@@ -372,7 +372,7 @@ def xb_bins = 10
 for(int xBi=0;xBi<=12;xBi++){
 	for(int q2i=0;q2i<=12;q2i++){
 		def title = "${((xBi)/xb_bins).round(2)} < xB < ${((xBi+1)/xb_bins).round(2)}_ ${q2i/2} < q2 < ${q2i/2+0.5}"
-		print("title is $title and q2i is $q2i with ${q2i/2}")
+		printer("title is $title and q2i is $q2i with ${q2i/2}",1)
 		out.addDataSet(Hist_beta_p[title])
 		out.addDataSet(Hist_beta_T[title])
 	}
