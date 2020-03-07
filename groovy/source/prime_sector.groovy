d573ff7245179065f3ddb6b8c0333ae074fe6fbf
robertej19 <robertej@mit.edu>
2020-03-06 20:11:34 -0500

    ""

--- groovy/source/prime_sector.groovy
+++ groovy/source/prime_sector.groovy
@@ -215,11 +215,11 @@ When it comes to presenting, this will be the first question.
 						}
 
 						def xb_bins = 7
-						def q2Round = Math.round(-qvec.mass2())
+						def q2Round = Math.round(-qvec.mass2()+0.5)
 						def xBRound = Math.round(xb_bins*xBjorken)
 						printer("Q2 is ${-qvec.mass2()} = $q2Round and xB is $xBjorken = $xBRound",2)
 
-						def title = "${((xBRound-0.5)/xb_bins).round(2)} < xB < ${((xBRound+0.5)/xb_bins).round(2)}_ ${q2Round - 0.5} < q2 < ${q2Round + 0.5}"
+						def title = "${((xBRound-0.5)/xb_bins).round(2)} < xB < ${((xBRound+0.5)/xb_bins).round(2)}_ ${q2Round} < q2 < ${q2Round}"
 						printer("Associated title is $title",2)
 						Hist_beta_p[title].fill(xBjorken,-qvec.mass2())
 
