ac33c2886b410f98599f56ac7247738a9b7c4a6f
Robert Johnston <robertej@ifarm1402.jlab.org>
2020-03-14 14:31:23 -0400

    ""

--- groovy/source/runit.sh
+++ groovy/source/runit.sh
@@ -1,5 +1,5 @@
-../scripts/this_run_groovy_4_args.sh prime_sector.groovy /lustre19/expphy/volatile/clas12/rg-a/production/pass0/physTrain/dst/train/skim8/ $1 $2
+#../scripts/this_run_groovy_4_args.sh prime_sector.groovy /lustre19/expphy/volatile/clas12/rg-a/production/pass0/physTrain/dst/train/skim8/ $1 $2
 
-#../scripts/this_run_groovy_4_args.sh prime_sector.groovy /work/clas12/rg-a/trains/v16_v2/skim8_ep/ $1 $2
+../scripts/this_run_groovy_4_args.sh prime_sector.groovy /work/clas12/rg-a/trains/v16_v2/skim8_ep/ $1 $2
 ../scripts/view_hipo.sh output_file_histos.hipo
 
