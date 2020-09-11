adf82d2f21bac67ac0e477576874f1ea1dd0987f
robertej19 <robertej@mit.edu>
2020-09-10 22:25:06 -0400

    updated

--- groovy/src/main_ana_multi.groovy
+++ groovy/src/main_ana_multi.groovy
@@ -72,6 +72,7 @@ def screen_updater(FileStartTime,CurrentCounter,CountRate,NumTotalCounts,filenam
 }
 
 
+
 def processEvent(event,hxB,fcc_final) {
 	def beam = LorentzVector.withPID(11,0,0,10.6)
 	def target = LorentzVector.withPID(2212,0,0,0)
@@ -90,37 +91,181 @@ def processEvent(event,hxB,fcc_final) {
 			println("fcup charge increashing to "+ fcupBeamCharge)
 			fcc_final = fcupBeamCharge
 				}
-		//fcc_final = 3
+
+		//println partb.getInt('status')
+
+
+
+/*
+if bankname[scint][particle_index] == 4 OR particle.status > 4000
+	proton in CD
+if bankname[scint][particle_index] == 12 OR particle.status > 4000
+	proton in FD
+
+		def particle_stati = recon_Particles.getInt('status')
+
+		def scint_sectors = [recon_Scint.getShort('pindex')*.toInteger(), recon_Scint.getInt('sector')].transpose().collectEntries()
+		def scint_detectors = [recon_Scint.getShort('pindex')*.toInteger(), recon_Scint.getInt('detector')].transpose().collectEntries()
+
+		def recon_Particles = event.getBank("REC::Particle")
+		def recon_Cal = event.getBank("REC::Calorimeter")
+		def recon_Scint = event.getBank("REC::Scintillator")
+
+		if(scint_detectors[particle_index]==4){
+			printer("particle detected in CTOF",0)
+
+			1000×FT + 2000×FD + 4000×CD
+where FT/FD/CD are 1 if that detector subsystem contributed to the particle, else 0
+
+Going forward I would encourage always separating any distribution into those two regions, especially when looking at particle kinematics.
+I would suggest that in the title where you have "Electron" and "Proton" to add either FD or FTOF, CD or CTOF. to help clarify this.
+When it comes to presenting, this will be the first question.
+*/
+
+
+
+
 		def index_of_electrons_and_protons = (0..<partb.rows()).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0}
 			.collectMany{iele->(0..<partb.rows()).findAll{partb.getInt('pid',it)==2212}.collect{ipro->[iele,ipro]}
 		}
 		printerUtil.printer("index_of_electrons_and_protons "+index_of_electrons_and_protons,0)
 
+		def index_of_pions = (0..<partb.rows()-1).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
+			.findAll{ig1->'xyz'.collect{partb.getFloat("p$it",ig1)**2}.sum()>0.16}
+			.collectMany{ig1->
+			(ig1+1..<partb.rows()).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
+			.findAll{ig2->'xyz'.collect{partb.getFloat("p$it",ig2)**2}.sum()>0.16}
+			.collect{ig2->[ig1,ig2]}
+		}
+		printerUtil.printer("index of pions is " + index_of_pions,0)
 
 		def isep0s = index_of_electrons_and_protons.findAll{iele,ipro->
 			def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
 			def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
 			printerUtil.printer("first electron is"+ele,0)
 
+			if(event.hasBank("MC::Particle")) {
+				printer("Event has MC Particle bank!",1)
+				def mcb = event.getBank("MC::Particle")
+				def mfac = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 3.2 : 2.5
+
+				def profac = 0.9
+
+				//mfac=1
+				profac = 1.0
+
+				ele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0) + (partb.getFloat(it,iele)-mcb.getFloat(it,0))*mfac})
+				pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{profac*(mcb.getFloat(it,1) + (partb.getFloat(it,ipro)-mcb.getFloat(it,1))*mfac)})
+				printerUtil.printer("second electron is"+ele)
+				def evec = new Vector3()
+				evec.setMagThetaPhi(ele.p(), ele.theta(), ele.phi())
+				def pvec = new Vector3()
+				pvec.setMagThetaPhi(pro.p(), pro.theta(), pro.phi())
+			}
+
+			def eletheta = Math.toDegrees(ele.theta())
+			def protheta = Math.toDegrees(pro.theta())
+			if(protheta<0) protheta+=360
 
 
-		
 
+			def wvec = beam+target-ele
 			def qvec = beam-ele
+			def epx = beam+target-ele-pro
+			//def t_sqrt = pro - target //t = (p'-p)^2
+			//def t_MomTran = t_sqrt.vect().dot(t_sqrt.vect())
 
-
-
+			printerUtil.printer("epx mass squared is:${epx.mass2()}",0)
 			def xBjorken = -qvec.mass2()/(2*pro.vect().dot(qvec.vect()))
 			printerUtil.printer("adding XB to hist "+index_of_electrons_and_protons,0)
-
 			hxB.fill(xBjorken)
 
-			
-		
+			def pdet = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 'FD':'CD'
+
+			def profi = Math.toDegrees(pro.phi())
+			if(profi<0) profi+=360
+
+			def esec = (0..<scib.rows()).find{scib.getShort('pindex',it)==iele}?.with{scib.getByte('sector',it)}
+			def psec = (0..<scib.rows()).find{scib.getShort('pindex',it)==ipro}?.with{scib.getByte('sector',it)}
+			if(psec==0) {
+				psec = Math.floor(profi/60).toInteger() +2
+				if(psec==7) psec=1
+			}
+
+			def isep0 = epx.mass2()<1 && wvec.mass()>2
+
+			def pi0s = index_of_pions.collect{ig1,ig2->
+				def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
+				def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
+				if(ele.vect().theta(g1.vect())>8 && ele.vect().theta(g2.vect())>8) {
+					def gg = g1+g2
+					def ggmass = gg.mass()
+					def ispi0 = ggmass<0.2 && ggmass>0.07// && gg.p()>1.5
+
+					if(ispi0) {
+						def epggx = epx-gg
+						def thetaXPi = epx.vect().theta(gg.vect())
+						def dpt0 = epggx.px().abs()<0.3 && epggx.py().abs()<0.3
+						def dmisse0 = epggx.e()<1
+						def tt0 = -(pro-target).mass2()
+						def procalc = beam+target-ele-gg
+						def tt1 = -(procalc-target).mass2()
+
+
+						def vLept = beam.vect().cross(ele.vect())
+						def vHad = pro.vect().cross(gg.vect())
+						def PlaneDot = vLept.dot(vHad)
+						def cosangle = PlaneDot/vLept.mag()/vHad.mag()
+						def LeptHadAngle = Math.toDegrees( Math.acos(cosangle))
+						if (pro.vect().dot(vLept)<0){
+							LeptHadAngle = -LeptHadAngle+360
+						}
+
+						def xb_bins = 10
+						def q2Round = Math.round((-qvec.mass2())*2+0.5)/2
+						def xBRound = Math.round(xb_bins*xBjorken+0.5)
+						printerUtil.printer("Q2 is ${-qvec.mass2()} = $q2Round and xB is $xBjorken = $xBRound",0)
+
+						if (q2Round < 0.4){
+							printer("Q2 is low at $q2round",2)
+						}
+
+						def title = "${((xBRound-1)/xb_bins).round(2)} < xB < ${((xBRound)/xb_bins).round(2)}_ ${q2Round - 0.5} < q2 < ${q2Round+0.0}"
+						//printer("Associated title is $title",2)
+
+						//println(t_bins)
+						def tRound = (Math.round(tt0*10)/10)
+						def UltraTitle = "88"
+
+						def TitleUltra = 0
+
+						if(isep0){
+							if(ispi0 && isep0 && dmisse0 && dpt0 && thetaXPi<2){
+
+
+
+								//if(TitleUltra==0){
+							//		println("Ultra is 0, t is $tt0")
+							//	}
+
+
+								//num_dvpp_events = num_dvpp_events + 1
+							
+								dvmp_counts = dvmp_counts +1
+								//println("num dvmp counts is $dvmp_counts")
+							}
+						}
+					}
+					//return ispi0
+				}
+			}
+			//return false
 		}
 	}
 	return fcc_final
 }
+
+
 
 
 
@@ -176,6 +321,8 @@ def CalcLumi(qFcup) {
 }
 
 
+total_counts = 0
+lumi_total = 0
 GParsPool.withPool 6, {
 	FilesToProcess.eachParallel{fname->
 		def reader = new HipoDataSource()
@@ -184,6 +331,8 @@ GParsPool.withPool 6, {
 		reader.open(fname)
 		printerUtil.printer("processing file ${fname}",1)
 		def NumEventsInFile= reader.getSize().toInteger()
+		
+		dvmp_counts = 0
 		def NumEventsToProcess = DesiredNumEventsToProcess
 		if (DesiredNumEventsToProcess == 0){NumEventsToProcess = NumEventsInFile}
 		//printerUtil.printer("\n \n $fname has ${(NumEventsToProcess/Mil).round(2)} M events, is file number ${i+1} of ${FilesToProcess.size()}",2)
@@ -201,11 +350,18 @@ GParsPool.withPool 6, {
 			screen_updater(FileStartTime,evcount.get(),CountRate.toInteger(),NumEventsToProcess,fname)
 			def event = reader.getNextEvent()
 			fcc_final = processEvent(event,hxB,fcc_final)
+			//println(dvmp_counts)
 		}
+
 
 		println("processing file ${fname}")
 		println("final fcup charge in nc: "+fcc_final)
 		println(CalcLumi(fcc_final))
+
+		total_counts = total_counts + dvmp_counts
+		println("total caounts of dvmp is " + total_counts)
+		lumi_total = lumi_total + CalcLumi(fcc_final)
+		println("total lumi is " + lumi_total)
 
 		endtime = new Date()
 		def TotalFileRunTime = (endtime.getTime() - FileStartTime)/1000/60
@@ -229,6 +385,8 @@ GParsPool.withPool 6, {
 }
 
 
+println("final total caounts of dvmp is " + total_counts)
+println("final total lumi is " + lumi_total)
 
 def dateX = new Date()
 printerUtil.printer("Finished processing all files at ${dateX.format('HH:mm:ss')}",1)
---
+++ groovy/src/main_ana_multi_working.groovy
@@ -0,0 +1,244 @@
+#!/usr/bin/groovy
+
+//From Java
+import java.io.*
+import java.util.*
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+import java.util.Date
+
+
+//From JLab
+import org.jlab.clas.physics.LorentzVector
+import org.jlab.clas.physics.Vector3
+import org.jlab.detector.base.DetectorType
+import org.jlab.groot.base.GStyle
+import org.jlab.groot.data.GraphErrors
+import org.jlab.groot.data.H1F
+import org.jlab.groot.data.H2F
+import org.jlab.groot.data.TDirectory
+import org.jlab.groot.fitter.DataFitter
+import org.jlab.groot.graphics.EmbeddedCanvas
+import org.jlab.groot.group.DataGroup
+import org.jlab.groot.math.F1D
+import org.jlab.io.base.DataBank
+import org.jlab.io.base.DataEvent
+import org.jlab.io.hipo.HipoDataSource
+import org.jlab.io.hipo.HipoDataSync
+
+//From Groovy
+import groovyx.gpars.GParsPool
+
+
+//From Local
+import utils.FileGetter
+import utils.Printer
+
+
+MyMods.enable() //I don't know what this does, its from Andrey, don't touch it, it works
+
+println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+
+Mil = 1000000
+
+dvmp_counts = 0
+
+def fg = new FileGetter()
+printerUtil = new Printer() //This needs to be defined as global (no def before the object name) so that it can be used in all methods
+
+def screen_updater(FileStartTime,CurrentCounter,CountRate,NumTotalCounts,filename){
+	if(CurrentCounter % CountRate == 0){
+		runtime = new Date()
+		TimeElapsed = (runtime.getTime() - FileStartTime)/1000/60
+		CountsLeft = NumTotalCounts-CurrentCounter
+		TimeLeft = TimeElapsed/CurrentCounter*CountsLeft
+		Rate = CurrentCounter/TimeElapsed/1000/60
+		uTS = Math.round(TimeLeft*60+runtime.getTime()/1000)
+		eta = Date.from(Instant.ofEpochSecond(uTS)).format('HH:mm:ss')
+
+		//printerUtil.printer("Total running time in minutes is: ${TimeElapsed.round(2)}",2)
+		printerUtil.printer("${(CurrentCounter/Mil).round(2)}M events have been processed from ${filename}, ${(CountsLeft/Mil).round(2)}M events remain",2)
+		printerUtil.printer("Processing Rate is ${Rate.round(1)} kHz",2)
+		printerUtil.printer("Anticipated file finish time is $eta, ${TimeLeft.round(2)} minutes left",1)
+		return TimeLeft
+	}
+}
+
+
+def processEvent(event,hxB,fcc_final) {
+	def beam = LorentzVector.withPID(11,0,0,10.6)
+	def target = LorentzVector.withPID(2212,0,0,0)
+
+	def banknames = ['REC::Event','REC::Particle','REC::Cherenkov','REC::Calorimeter','REC::Traj','REC::Track','REC::Scintillator']
+
+	if(banknames.every{event.hasBank(it)}) {
+		def (evb,partb,cc,ec,traj,trck,scib) = banknames.collect{event.getBank(it)}
+		def banks = [cc:cc,ec:ec,part:partb,traj:traj,trck:trck]
+		def ihel = evb.getByte('helicity',0)
+		printerUtil.printer("ihel is "+ihel,0)
+
+		def fcupBeamCharge = evb.getFloat('beamCharge',0)
+
+		if(fcupBeamCharge > fcc_final){
+			println("fcup charge increashing to "+ fcupBeamCharge)
+			fcc_final = fcupBeamCharge
+				}
+		//fcc_final = 3
+		def index_of_electrons_and_protons = (0..<partb.rows()).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0}
+			.collectMany{iele->(0..<partb.rows()).findAll{partb.getInt('pid',it)==2212}.collect{ipro->[iele,ipro]}
+		}
+		printerUtil.printer("index_of_electrons_and_protons "+index_of_electrons_and_protons,0)
+
+
+		def isep0s = index_of_electrons_and_protons.findAll{iele,ipro->
+			def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
+			def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
+			printerUtil.printer("first electron is"+ele,0)
+
+
+
+		
+
+			def qvec = beam-ele
+
+
+
+			def xBjorken = -qvec.mass2()/(2*pro.vect().dot(qvec.vect()))
+			printerUtil.printer("adding XB to hist "+index_of_electrons_and_protons,0)
+
+			hxB.fill(xBjorken)
+
+			
+		
+		}
+	}
+	return fcc_final
+}
+
+
+
+def hxB = new H1F("Hist_xB","Bjorken x Distribution",1000,0,1.5)
+
+if (args.size()<3) {
+	printerUtil.printer("You need to include the number of events and files you want to process in the start command!",1)
+	printerUtil.printer("For example, <run-groovy filename.groovy hipo_file_to_process.hipo 1000 10",1)
+}
+
+
+
+def NumFilesToProcess = args[2].toInteger()
+
+def FilesToProcess = fg.GetFile(args[0]).take(NumFilesToProcess)
+
+def DesiredNumEventsToProcess = args[1].toInteger()
+printerUtil.printer("The following files have been found: ",1)
+def total_file_length = 0
+def file_length_processed = 0
+for (FileName in FilesToProcess){
+	total_file_length += FileName.length()
+	printerUtil.printer("$FileName",2)
+}
+
+
+
+printerUtil.printer((total_file_length/Mil/1000).round(2)+"G is the total file size in Bites (?)",2)
+
+def TotalNumEventsProcessed = 0
+def TotalRunTime = 0
+def num_ep_events = 0
+def num_dvpp_events = 0
+
+
+
+starttime = new Date()
+def procStartTime = starttime.getTime()
+
+
+def CalcLumi(qFcup) {
+	N_A = 6.02214E23 //avogadro's number
+	l = 5 //length of target, in centimeters
+	rho = 0.07 //density of liquid hydrogen target, in g/cm^3
+	e = 1.602E-19 //charge of an electron, in Columbs
+
+	Q = qFcup*1E-9 //charge is in units of nC, need to convert to Coulombs
+
+	L = N_A*l*rho*Q/e
+
+	return L //The luminosity that is returned has units of cm^-2
+
+}
+
+
+GParsPool.withPool 6, {
+	FilesToProcess.eachParallel{fname->
+		def reader = new HipoDataSource()
+		def Float fcc_final = 0
+		//def fname = FilesToProcess[i]
+		reader.open(fname)
+		printerUtil.printer("processing file ${fname}",1)
+		def NumEventsInFile= reader.getSize().toInteger()
+		def NumEventsToProcess = DesiredNumEventsToProcess
+		if (DesiredNumEventsToProcess == 0){NumEventsToProcess = NumEventsInFile}
+		//printerUtil.printer("\n \n $fname has ${(NumEventsToProcess/Mil).round(2)} M events, is file number ${i+1} of ${FilesToProcess.size()}",2)
+		def evcount = new AtomicInteger()
+		evcount.set(0)
+
+		def date = new Date()
+		def FileStartTime = date.getTime()
+		printerUtil.printer("Process file $fname at ${date.format('HH:mm:ss')}",1)
+
+
+		def CountRate = NumEventsToProcess/10
+		for (int j=0; j < NumEventsToProcess; j++) {
+			evcount.getAndIncrement()
+			screen_updater(FileStartTime,evcount.get(),CountRate.toInteger(),NumEventsToProcess,fname)
+			def event = reader.getNextEvent()
+			fcc_final = processEvent(event,hxB,fcc_final)
+		}
+
+		println("processing file ${fname}")
+		println("final fcup charge in nc: "+fcc_final)
+		println(CalcLumi(fcc_final))
+
+		endtime = new Date()
+		def TotalFileRunTime = (endtime.getTime() - FileStartTime)/1000/60
+		printerUtil.printer("Finished processing ${(NumEventsToProcess/Mil).round(2)} M events at ${date.format('HH:mm:ss')},total run time ${TotalFileRunTime.round(2)} minutes",1)
+		reader.close()
+
+		TotalRunTime += TotalFileRunTime
+		TotalNumEventsProcessed += NumEventsToProcess
+		//printerUtil.printer("Processed ${(i+1)} files, ${(TotalNumEventsProcessed/Mil).round(2)} M events, have ${FilesToProcess.size()-i-1} files left to process",1)
+
+		//def TotalTimeLeft = TotalRunTime*(FilesToProcess.size()-i-1)/(i+1)
+		//file_length_processed += fname.length()
+		//def TotalTimeLeft = TotalRunTime*(total_file_length-file_length_processed)/file_length_processed
+		//uTSX = Math.round(TotalTimeLeft*60+endtime.getTime()/1000)
+		//def etaTotal = Date.from(Instant.ofEpochSecond(uTSX)).format('HH:mm:ss')
+		//p//rinter("Total Run Time of ${TotalRunTime.round(2)} minutes, approximate finish time at ${etaTotal} ",1)
+		//printerUtil.printer("Total num DVMP counts so far is $dvmp_counts",2)
+		reader.close()
+
+	}
+}
+
+
+
+def dateX = new Date()
+printerUtil.printer("Finished processing all files at ${dateX.format('HH:mm:ss')}",1)
+def OutFileName = "output_file_histos"
+TDirectory out = new TDirectory()
+out.mkdir('/'+OutFileName)
+out.cd('/'+OutFileName)
+out.addDataSet(hxB)
+out.writeFile(" ${OutFileName} ${dateX.format('HH:mm:ss')}.hipo")
+
+//printerUtil.printer("confirm start time of ${procStartTime.format('HH:mm:ss')}",1)
+
+//exe.shutdown()
